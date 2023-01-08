3737ee4774ff1b3af73df5bdbba66936f6e2d543
umakkauturu <umakakuturu925@gmail.com>
2023-01-08 15:57:34 +0530

    Added consumer code

---
+++ src/main/java/com/ram/KafkaConsumerDemo/Consumer/KafkaConsumerImpl.java
@@ -0,0 +1,14 @@
+package com.ram.KafkaConsumerDemo.Consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerImpl {
+    @KafkaListener(topics = "uploadsheettopic", groupId = "Group1")
+    public void listen(String sheetName) {
+        if (sheetName.equals("hotels")) {
+            System.out.println("Received '" + sheetName + "' from the uploadsheettopic. in hotels");
+        }
+    }
+}
